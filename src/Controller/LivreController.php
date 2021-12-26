71d138cc7545b0b0a7ccb816b2dcfdede245b019
maarouf-ouassime <maaroufouassime@gmail.com>
2021-12-26 17:24:46 +0100

    hh

---
+++ src/Controller/LivreController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LivreController extends AbstractController
+{
+    #[Route('/livre', name: 'livre')]
+    public function index(): Response
+    {
+        return $this->render('livre/index.html.twig', [
+            'controller_name' => 'LivreController',
+        ]);
+    }
+}
